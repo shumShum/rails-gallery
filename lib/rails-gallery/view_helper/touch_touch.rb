a13c7ef0ff4fa4739b11bfe3f9fb59808453b3a9
Shumilkin Gena <shumilkin.gena@yandex.ru>
2014-03-18 17:36:56 +0400

    add block to helper

--- lib/rails-gallery/view_helper/touch_touch.rb
+++ lib/rails-gallery/view_helper/touch_touch.rb
@@ -2,7 +2,8 @@ module RailsGallery
   module ViewHelper
     module TouchTouch
       def touchgal_image photo, options = {}
-        content_tag :a, nil, options.merge(href: photo.path, title: photo.title)
+        block = block_given? ? yield : nil
+        content_tag :a, block, options.merge(href: photo.path, title: photo.title)
       end
       alias_method :touch_gallery_image, :touchgal_image
     end
